504e39305b8ad35a6dcb821df37905aa179b9801
Devin Jameson <devinjameson@me.com>
2020-10-09 15:14:34 -0400

    WIP

--- src/navigation/ModalStack.tsx
+++ src/navigation/ModalStack.tsx
@@ -4,8 +4,10 @@ import {
   HeaderBackButton,
   TransitionPresets,
 } from "@react-navigation/stack"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { useTranslation } from "react-i18next"
+import { SvgXml } from "react-native-svg"
 
 import { Stacks, ModalStackScreens, HomeStackScreens } from "./index"
 import LanguageSelection from "../modals/LanguageSelection"
@@ -20,13 +22,20 @@ import ExposureNotificationsInfo from "../Home/ExposureNotificationsInfo"
 import LocationInfo from "../Home/LocationInfo"
 import CallbackStack from "./CallbackStack"
 
-import { Headers, Colors } from "../styles"
+import {
+  Headers,
+  Colors,
+  Iconography,
+  Typography,
+  Spacing,
+  Layout,
+} from "../styles"
+import { Icons } from "../assets"
 
-const headerLeft = () => <HeaderLeft />
-const HeaderLeft = () => {
+const headerLeftBackButton = () => <HeaderLeftBackButton />
+const HeaderLeftBackButton = () => {
   const navigation = useNavigation()
   const { t } = useTranslation()
-
   return (
     <HeaderBackButton
       tintColor={Colors.primary150}
@@ -36,11 +45,53 @@ const HeaderLeft = () => {
   )
 }
 
+const modalHeader = () => <ModalHeader />
+const ModalHeader = () => {
+  const { t } = useTranslation()
+  const navigation = useNavigation()
+
+  return (
+    <View style={style.container}>
+      <Text numberOfLines={10} style={style.headerText}>
+        {t("screen_titles.protect_privacy")}
+      </Text>
+      <TouchableOpacity
+        onPress={navigation.goBack}
+        hitSlop={{ top: 30, right: 30, bottom: 30, left: 30 }}
+      >
+        <SvgXml
+          xml={Icons.XInCircle}
+          fill={Colors.neutral30}
+          width={Iconography.small}
+          height={Iconography.small}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  container: {
+    width: "100%",
+    paddingTop: Spacing.massive,
+    padding: Spacing.large,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    backgroundColor: Colors.secondary10,
+  },
+  headerText: {
+    ...Typography.header1,
+    color: Colors.primary125,
+    maxWidth: Layout.screenWidth * 0.8,
+  },
+})
+
 const Stack = createStackNavigator()
 
 const ModalStack: FunctionComponent = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator headerMode="screen" screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name={ModalStackScreens.LanguageSelection}
         component={LanguageSelection}
@@ -49,7 +100,11 @@ const ModalStack: FunctionComponent = () => {
       <Stack.Screen
         name={ModalStackScreens.ProtectPrivacy}
         component={ProtectPrivacy}
-        options={TransitionPresets.ModalTransition}
+        options={{
+          ...TransitionPresets.ModalTransition,
+          headerShown: true,
+          header: modalHeader,
+        }}
       />
       <Stack.Screen
         name={Stacks.AffectedUserStack}
@@ -94,7 +149,7 @@ const ModalStack: FunctionComponent = () => {
         component={ExposureDetectionStatus}
         options={{
           ...Headers.headerMinimalOptions,
-          headerLeft: headerLeft,
+          headerLeft: headerLeftBackButton,
         }}
       />
       <Stack.Screen
